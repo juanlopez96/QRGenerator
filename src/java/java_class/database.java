85abc159c1002f7133f2ab9586f83ceaaa577938
Juan <Juan@DESKTOP-CPPC97L>
2020-11-03 09:18:00 -0500

    Select con los tipos de vehiculos

--- src/java/java_class/database.java
+++ src/java/java_class/database.java
@@ -10,50 +10,104 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  *
  * @author Juan
  */
-public class database {
+public final class database {
 
     final private String url = "jdbc:oracle:thin:@localhost:1521:xe";
     final private String user = "system";
     final private String password = "123";
     Connection con;
 
+    //CONVERT TO FUNCTION AND CALL IT IN THE CONSTRUCTOR
     public database() {
+
+    }
+
+    public void connect() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             con = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Something went wrong!: " + e.getMessage());
         }
     }
 
-    public void diconnect() throws SQLException {
-        con.close();
+    public void disconnect() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Cannot disconnet from database " + e.getMessage());
+        }
     }
-    
-    public person getPerson(String id){
+
+    public person getPerson(String id) {
+        connect();
         person p1 = null;
-        try{
+        try {
             String sql = "SELECT * FROM PERSONA WHERE ID_PERSONA = ?";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 p1 = new person();
                 p1.setId(id);
                 p1.setName(rs.getString(2));
                 p1.setLastName(rs.getString(3));
             }
-            return p1;
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             System.out.println("Error when try to get person: " + e.getMessage());
-            return null;
+            p1 = null;
         }
-        
+        disconnect();
+        return p1;
+    }
+
+    public String getRol(String id) {
+        connect();
+        String rol = null;
+        try {
+            String sql = "SELECT NOMBRE_ROL FROM ROL "
+                    + "INNER JOIN PERSONA_ROL ON PERSONA_ROL.ID_ROL = ROL.ID_ROL "
+                    + "WHERE PERSONA_ROL.ID_PERSONA=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                rol = rs.getString(1);
+            }
+            return rol;
+        } catch (SQLException e) {
+            System.out.println("Cannot get rol " + e.getMessage());
+            rol = null;
+        }
+        disconnect();
+        return rol;
+    }
+
+    public Map getType() {
+        connect();
+        Map<String,String>vehicle_type = new HashMap();
+        try {
+            String sql = "SELECT * FROM TIPO";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                vehicle_type.put(rs.getString(1), rs.getString(2));
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("Cannot get data from TIPO: " + e.getMessage());
+        }
+        disconnect();
+        return vehicle_type;
     }
 }
--- web/autenticate.jsp
+++ web/autenticate.jsp
@@ -4,6 +4,9 @@
     Author     : Juan
 --%>
 
+<%@page import="java.util.Iterator"%>
+<%@page import="java.util.Map"%>
+<%@page import="java.util.HashMap"%>
 <%@page import="java_class.person"%>
 <%@page import="java_class.database"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
@@ -11,20 +14,49 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Registro</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Universidad de Cundinamarca</h1>
         <% 
             database db = new database();
             String id = request.getParameter("identificacion");
             String name = "";
             String last_name = "";
+            String rol = "";
             person p1 = db.getPerson(id);
+            Map vehicle_type = db.getType();
             if(p1!=null){
                 name = p1.getName();
                 last_name = p1.getLastName();
-                %>Bienvenido <%=name%> <%=last_name%>
+                rol = db.getRol(id);
+                %>
+                <label>Documento de identidad:</label>
+                <input readonly value="<%=p1.getId() %>"/>
+                <label>Nombre:</label>
+                <input readonly value="<%=p1.getName()%>"/>
+                <label>Apellido:</label>
+                <input readonly value="<%=p1.getLastName()%>"/>
+                <label>Rol:</label>
+                <input readonly value="<%=rol%>"/>
+                <br>
+                <p><button>Registrar vehículo</button></p>
+                <form>
+                    <div>
+                        <label>Placa:</label>
+                        <input type="text" min="4" max="6"/>
+                        <label>Tipo:</label>
+                        <select>
+                            <%
+                                Iterator iterator = vehicle_type.entrySet().iterator();
+                                while(iterator.hasNext()){
+                                    Map.Entry entry = (Map.Entry)iterator.next();%>
+                                    <option value="<%=entry.getKey()%>"><%=entry.getValue()%></option>
+                                <%} 
+                            %>
+                        </select>
+                    </div>
+                </form>
                 <%
             }
             else{
